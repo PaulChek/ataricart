07dd4b801236a56d1f0d4f92fd3b6326f8c1f186
PaulChek <paulchek777@hotmail.com>
2023-06-14 00:14:50 +0700

    falling apple

--- vertical_positioning/vert_pos.asm
+++ vertical_positioning/vert_pos.asm
@@ -26,7 +26,7 @@ Reset:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initializing the variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;     
-    ldx #152
+    ldx #180
     stx P0Ypos ; P0Ypos = 180
     ldx #9
     stx P0Height ; Poheight = 9
@@ -44,8 +44,8 @@ StartFrame:
 
     lda #0
     sta VSYNC
-   
     ldy #37
+
 VBLANKloop:
     dey
     sta WSYNC    
@@ -57,25 +57,12 @@ VBLANKloop:
 ;; Drawing visible part of screen 192 lines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
            ldx #192
-ScanLineLoop:          
-           txa  
-	   sec
-           sbc P0Ypos
-           cmp P0Height
-           bcc P0Loop
-           lda #0
-P0Loop:		
-	   tay	
-           lda P0Bitmap,Y
-           sta GRP0
-           lda P0Colors,Y
-           sta COLUP0
-           sta WSYNC
-           dex
-           bne ScanLineLoop
-           
-           ;lda #0
-	   ;sta GRP0	
+LoopVisible:          
+           stx WSYNC
+           dex 
+           bne LoopVisible
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Drawing 30 overscan lines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;            
@@ -90,40 +77,28 @@ OverscanLoop:
        lda #0
        sta VBLANK
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Decrement player position
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;      
-
-	dec P0Ypos
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Loop the frame
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;       
-       
-       jmp StartFrame
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Lookup table the player graphicc bitmap
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 P0Bitmap:
-    byte #%00000000
-    byte #%00010000
-    byte #%00010000
-    byte #%01111110
+    byte #%00000100
+    byte #%00011100
+    byte #%00001000
+    byte #%01111111
+    byte #%11111110
     byte #%11111100
-    byte #%11111100
-    byte #%11111100
-    byte #%01111110
+    byte #%11111110
+    byte #%01111111
     byte #%00101100
 P0Colors:
+    byte #$00
     byte #$40
-    byte #$D2
-    byte #$D2
     byte #$40
     byte #$40
     byte #$40
     byte #$42
     byte #$42
     byte #$44
- 
+    byte #$D2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; End of catridge
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
